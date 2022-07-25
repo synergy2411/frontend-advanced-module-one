485e546bf9e03caa0c77043c8d511930b8143037
Sumit K <synergy2411@outlook.com>
2022-07-25 15:11:28 +0530

    intial commit "

---
+++ 01-basics/scripts/app.js
@@ -0,0 +1,179 @@
+// console.log(1 + '2' + 3 - 3);
+
+// console.log(2 + "1" + 1 - 1);
+
+
+// console.log(30 > 20 > 10 === false);
+
+// false Value -> undefined, null, 0, false
+
+// console.log(0 === false)
+// var x = 0;
+
+// var x = {};
+
+// var x = null;
+
+
+// if (x) {
+//     console.log("Greater than zero");
+// } else {
+//     console.log("Not greater than Zero");
+// }
+
+
+
+// console.log('20' === 20);
+
+
+// console.log(100 + ('25' - 25) + 10);
+
+
+// var x = y = 20;
+
+// console.log(x, y);
+
+// var x = 100;
+// console.log(typeof (x));
+
+// x = "HEllo"
+// console.log(typeof (x));
+
+// x = function () { }
+
+// console.log(typeof (x));
+
+// x = new Array(1, 2, 3, 4)
+// console.log(typeof (x));
+
+// x = { name: "Foo" }
+// console.log(typeof (x));
+
+
+
+// Creational Phase - allocates the memory 
+
+// Executional Phase - executes the code
+
+// let, const & var
+
+// function demo(arr) {
+//     if (arr.length > 2) {
+//         let load = "LOADING"
+//         console.log(flash);
+//     } else {
+//         let flash = "FLASHING"
+//     }
+// }
+
+// demo([2, 3, 4, 5]);
+
+
+
+// const USERNAME = "Foo Bar";
+
+// USERNAME = "Bar Bam";
+
+
+// const user = {
+//     name: "Foo"
+// }
+
+// user = {
+//     name: "Baz"
+// }
+
+// user.name = "Bar";
+
+// console.log(user);          // ?
+
+
+
+// const fruits = ["apples", "kiwi", "oranges"];
+
+// fruits.push("Guava")
+
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Operators & Precedence
+// Strings & Template Literals
+
+// let username = "John Doe";
+// let age = 32;
+
+
+// const str = `Hello there, I 'm ${username}
+
+
+// !
+
+// I'm ${age} years old
+// ..
+
+
+// `;
+
+// console.log(str);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Type Conversion & Coercion
+// const age = Number(prompt("Pls enter your year of birth"));
+
+// const retirementAge = 2037 + age;
+
+// console.log(retirementAge);
+
+
+
+
+
+
+// Conditional & Loops
+// Document Object Model
+
+
+const heading = document.querySelector(".heading");
+console.log(heading.innerHTML);
+heading.innerHTML = "Hello World";
