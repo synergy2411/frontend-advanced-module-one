a909f7342cb2e1adb11b425ba6db6007fe7f97b0
Sumit K <synergy2411@outlook.com>
2022-07-29 10:22:43 +0530

    misc topics

--- 03-dom-traversing/scripts/main.js
+++ 03-dom-traversing/scripts/main.js
@@ -34,3 +34,110 @@ ul.addEventListener("click", event => {
 //         document.querySelector(id).scrollIntoView({ behavior: "smooth" })
 //     })
 // })
+
+
+
+
+
+
+
+
+
+// for (let prop in window) {
+//     console.log(prop + " : " + window[prop]);
+// }
+
+// window.onload = () => {
+//     alert("Loaded...")
+// }
+
+
+// let userOne = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 32
+// }
+
+// let userThree = {
+//     lastName: "Maria"
+// }
+
+// let userTwo = {
+//     email: "user2@test.com",
+//     address: "201 Main road",
+//     age: 40
+// }
+
+// let newObj = Object.assign({}, userOne, userTwo, userThree)
+
+// console.log(newObj);
+
+
+// let newObjTwo = {
+//     ...userOne,
+//     ...userTwo,
+//     ...userThree
+// }
+
+// console.log(newObjTwo);
+
+// for (let key of Object.keys(user)) {
+//     console.log(key);
+// }
+// for (let value of Object.values(user)) {
+//     console.log(value);
+// }
+// for (let [key, value] of Object.entries(user)) {
+//     console.log(key + " - " + value);
+// }
+
+
+
+
+// console.log("Starting");
+// setTimeout(() => {
+//     console.log("3 Seconds passed away ");
+// }, 3000);
+
+// const butnStop = document.querySelector("#btn-stop")
+
+// let count = 0;
+// let notifier = setInterval(() => {
+//     ++count;
+//     console.log(count);
+// }, 1000)
+
+// butnStop.addEventListener("click", event => {
+//     clearInterval(notifier);
+// })
+
+
+// let employee = {
+//     company: "Deloitte"
+// }
+
+
+// let foo = {};
+
+// foo.__proto__ = employee;
+
+// console.log(foo.company);
+
+
+
+const idOne = Symbol("id")
+const idTwo = Symbol("id")
+
+// console.log(idOne === idTwo);
+
+let user = {
+    [idOne]: "userID1012",
+    sibling: true,
+    name: "John"
+}
+
+for (let prop in user) {
+    console.log(prop)
+}
+
+// console.log(user);
